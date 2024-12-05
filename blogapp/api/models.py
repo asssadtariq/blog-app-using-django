5f1537acef0e5b730f6fb21bd44d60ffe8653672
Asad Tariq <asadtariq.at66@gmail.com>
2024-12-05 22:38:20 +0500

    add: new models created - Blog and BlogType

--- blogapp/api/models.py
+++ blogapp/api/models.py
@@ -1,3 +1,26 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class BlogType(models.Model):
+    blog_type_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False
+    )
+    blog_type_name = models.CharField(null=False, max_length=128)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.blog_type_name
+
+
+class Blogs(models.Model):
+    blog_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    blog_title = models.CharField(null=False, max_length=256)
+    blog_content = models.TextField()
+    blog_type = models.ForeignKey(
+        BlogType, on_delete=models.CASCADE, related_name="blog_type"
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
+    created_on = models.DateTimeField(auto_now_add=True)
